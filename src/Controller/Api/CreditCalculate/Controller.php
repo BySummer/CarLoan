e471b2c44df0bcc75c62081d9466fb8709246239
s.ambroziev <s.ambroziev@affstar.com>
2025-01-23 03:30:06 +0500

    added missing extends AbstractController

--- src/Controller/Api/CreditCalculate/Controller.php
+++ src/Controller/Api/CreditCalculate/Controller.php
@@ -7,11 +7,12 @@ use App\Application\Response\ResponseJsonFactory;
 use App\Controller\Api\CreditCalculate\Dto\Input;
 use App\Controller\Api\CreditCalculate\Output\OutputService;
 use App\Repository\LoanProgramRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class Controller
+class Controller extends AbstractController
 {
     public function __construct(
         private readonly ValidatorInterface           $validator,
